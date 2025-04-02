3136e75199883bcb24b7d4e6c9d1d73013874366
Alexander Lavrushko <lavrushko92@gmail.com>
2025-04-02 23:22:49 +0200

    fix repo URL in scripts

--- github-create-release.sh
+++ github-create-release.sh
@@ -3,5 +3,5 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer <token>" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-      https://api.github.com/NullPointerCrew/TestGHRelease/releases \
-  -d '{"tag_name":"0.0.3","target_commitish":"main","name":"0.0.3","body":"Release notes:\n- fix(iOS): 123\nfix(Android): 123","draft":true,"prerelease":false,"generate_release_notes":false}'
+      https://api.github.com/repos/NullPointerCrew/TestGHRelease/releases \
+  -d '{"tag_name":"0.0.4","target_commitish":"main","name":"0.0.4","body":"Release notes:\n- Add script1\n- Add script2","draft":true,"prerelease":false,"generate_release_notes":false}'
--- github-list-releases.sh
+++ github-list-releases.sh
@@ -2,4 +2,4 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer <token>" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/NullPointerCrew/TestGHRelease/releases
+  https://api.github.com/repos/NullPointerCrew/TestGHRelease/releases
